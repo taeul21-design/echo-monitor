13341d9c434bb1e9a7a250423cbc691846dc6bc3
kimjy <taeul21@gmail.com>
2026-04-05 15:06:36 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+// Service Worker - 친환경 인증농산물 조회 앱
+const CACHE_NAME = 'eco-cert-v1';
+const ASSETS = [
+  './index.html',
+  './data.json',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).catch(() => cached);
+    })
+  );
+});
